51b609f1cdef47e76b3d197bf33e08186a0a166b
Samuel Sackey <smlscky@gmail.com>
2023-04-12 10:00:24 +0000

    Add exception handling to I/O functions

--- fileio.py
+++ fileio.py
@@ -3,11 +3,17 @@
 
 def write(text):
     """Write text to file(Overwrites)"""
-    with open("data.csv", mode="w") as data:
-        data.write(text)
+    try:
+        with open("data.csv", mode="w") as data:
+            data.write(text)
+    except Exception as e:
+        print("An error occurred while writing to the file.", e)
 
 
 def append(text):
     """Append text to file"""
-    with open("data.csv", mode="a") as data:
-        data.write(text + "\n")
+    try:
+        with open("data.csv", mode="a") as data:
+            data.write(text + "\n")
+    except Exception as e:
+        print("An error occurred while writing to the file.", e)
